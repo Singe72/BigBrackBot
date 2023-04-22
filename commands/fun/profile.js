cda6e3360617ad3339ab170647a2ce0a5ee807ec
Singe72 <=>
2023-04-22 23:43:39 +0200

    Started profile command

---
+++ commands/fun/profile.js
@@ -0,0 +1,36 @@
+const { AttachmentBuilder, SlashCommandBuilder } = require("discord.js");
+const { profileImage } = require("discord-arts");
+
+module.exports = {
+	cooldown: 5,
+	data: new SlashCommandBuilder()
+		.setName("profile")
+		.setDescription("Gérer son profil et afficher celui des autres membres")
+		.addSubcommand(subcommand =>
+			subcommand.setName("show")
+				.setDescription("Afficher le profil d'un membre")
+				.addUserOption(option =>
+					option.setName("membre")
+						.setDescription("Membre dont afficher le profil")
+						.setRequired(false)
+				)
+		)
+		.setDMPermission(true),
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		const subcommand = interaction.options.getSubcommand();
+
+		if (subcommand === "show") {
+			try {
+				const member = interaction.options.getMember("membre") ?? interaction.member;
+				const profileBuffer = await profileImage(member.id);
+				const imageAttachment = new AttachmentBuilder(profileBuffer, { name: "profile.png" });
+				await interaction.editReply({ files: [imageAttachment] });
+			} catch (error) {
+				console.error(error);
+				await interaction.editReply("Une erreur est survenue lors de la génération de votre profil.");
+			}
+		}
+	}
+};
