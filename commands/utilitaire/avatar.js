a7f82db72664a872f1acebfb66f306d4f4c6dc86
Singe72 <=>
2023-05-20 15:35:21 +0200

    Added avatar pagination

--- commands/utilitaire/avatar.js
+++ commands/utilitaire/avatar.js
@@ -1,7 +1,7 @@
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 const { clientColor } = require("../../config.json");
 const { simpleEmbed } = require("../../utils/embeds.js");
-
+const buttonPages = require("../../utils/pagination.js");
 
 module.exports = {
 	cooldown: 5,
@@ -12,7 +12,8 @@ module.exports = {
 			option.setName("membre")
 				.setDescription("Membre à afficher")
 				.setRequired(false)
-		),
+		)
+		.setDMPermission(false),
 	async execute(interaction) {
 		const user = interaction.options.getUser("membre") || interaction.user;
 		let member = interaction.options.getMember("membre");
@@ -28,11 +29,19 @@ module.exports = {
 		if (!user) return interaction.reply({ embeds: [simpleEmbed("Ce membre n'existe pas !")], ephemeral: true });
 		if (!member) return interaction.reply({ embeds: [simpleEmbed(`<@${user.id}> n'est pas membre du serveur !`)], ephemeral: true });
 
-		const embed = new EmbedBuilder()
+		const userAvatar = new EmbedBuilder()
 			.setColor(clientColor)
-			.setTitle(`Avatar de ${member.nickname || member.user.username}`)
-			.setImage(member.user.displayAvatarURL({ dynamic: true, size: 4096 }));
+			.setTitle(`Avatar de ${user.username}`)
+			.setImage(user.displayAvatarURL({ dynamic: true, size: 4096 }));
 
-		await interaction.reply({ embeds: [embed] });
+		if (!member.avatar) return interaction.reply({ embeds: [userAvatar] });
+
+		const memberAvatar = new EmbedBuilder()
+			.setColor(clientColor)
+			.setTitle(`Avatar de ${member.nickname || user.username}`)
+			.setImage(member.displayAvatarURL({ dynamic: true, size: 4096 }));
+
+		const pages = [userAvatar, memberAvatar];
+		buttonPages(interaction, pages, false);
 	}
 };
