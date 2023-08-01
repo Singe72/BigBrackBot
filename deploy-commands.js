088ae4c25b802756efc7a0cd9cf278293906d786
Singe72 <=>
2023-08-01 19:31:54 +0200

    made commands global

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require("discord.js");
-const { clientId, guildId, token } = require("./config.json");
+const { clientId, token } = require("./config.json");
 const fs = require("node:fs");
 const path = require("node:path");
 
@@ -30,7 +30,7 @@ const rest = new REST().setToken(token);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommands(clientId),
 			{ body: commands }
 		);
 
