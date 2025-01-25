590c2804e67bbb9fdadac7de0512d5cd239946e8
Shuttle <t.hyaumet@free.fr>
2025-01-25 16:06:00 +0100

    create nasa apod cron job

---
+++ cron/nasa.js
@@ -0,0 +1,57 @@
+const { CronJob } = require("cron");
+const { clientColor, nasa: api_key } = require("../config.json");
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("discord.js");
+
+const createNasaJob = (client) => {
+    return CronJob.from({
+        cronTime: "0 2 * * *",
+        onTick: async function () {
+            const url = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&concept_tags=True&thumbs=True`;
+            const channelId = "656568484448305173";
+
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+
+                if (data.media_type === "image") {
+                    const embed = new EmbedBuilder()
+                        .setColor(clientColor)
+                        .setTitle("Image du jour de la NASA")
+                        .setDescription(`
+                            __${data.title}__ ${data.date}
+                            ${data.copyright ? `*© ${data.copyright.replace(/\n/g, "")}*` : ""}
+                        `)
+                        .setImage(data.hdurl ? data.hdurl : data.url)
+                        .setFooter({ text: "Source : NASA" });
+
+                    return client.channels.cache.get(channelId).send({ embeds: [embed] });
+                } else if (data.media_type === "video") {
+                    const embed = new EmbedBuilder()
+                        .setColor(clientColor)
+                        .setTitle("Image du jour de la NASA")
+                        .setDescription(`
+                            __${data.title}__ ${data.date}
+                            ${data.copyright ? `*© ${data.copyright}*` : ""}
+                        `)
+                        .setImage(data.thumbnail_url)
+                        .setFooter({ text: "Source : NASA" });
+
+                    const button = new ButtonBuilder()
+                        .setLabel("Voir la vidéo")
+                        .setURL(data.url)
+                        .setStyle(ButtonStyle.Link);
+
+                    const row = new ActionRowBuilder().addComponents(button);
+
+                    return client.channels.cache.get(channelId).send({ embeds: [embed], components: [row] });
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        start: false,
+        timeZone: "Europe/Paris",
+    });
+};
+
+module.exports = createNasaJob;
--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,5 @@
 const { Events } = require("discord.js");
+const createNasaJob = require("../../cron/nasa");
 
 module.exports = {
 	name: Events.ClientReady,
@@ -6,5 +7,9 @@ module.exports = {
 	execute(client) {
 		client.application.commands.fetch();
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		
+		const nasaJob = createNasaJob(client);
+		nasaJob.start();
+		console.log("NASA job started");
 	}
 };
