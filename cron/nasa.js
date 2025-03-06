ae8d3b288244f6116f42a33148ff639ed8613298
Singe72 <t.hyaumet@free.fr>
2025-03-06 09:24:04 +0100

    fix cron time

--- cron/nasa.js
+++ cron/nasa.js
@@ -4,7 +4,7 @@ const { EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("
 
 const createNasaJob = (client) => {
     return CronJob.from({
-        cronTime: "0 3 * * *",
+        cronTime: "30 6 * * *",
         onTick: async function () {
             const url = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&concept_tags=True&thumbs=True`;
             const channelId = "656568484448305173";
